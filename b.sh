2d6c50472aac1284ee46a3cde6bc4071e3dc6489
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2020-12-09 09:04:55 +0300

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# parasitical v1.0
+
+# coded by: https://github.com/iraq-hacker/Contact-spam
+
+# Telegram : https://t.me/iiwiw
+
+# Telegram channel : https://t.me/iiraqNew
+
+#Dev : ReKuShE 
+
+trap 'printf "\n";stop' 2
+
+banner() {
+
+ 
+
+printf "\e[1;92m _____       ___   _____        ___   _____   _   _____   _   _____       ___   _      \e[0m\n"
+
+printf "\e[1;92m |  _  \     /   | |  _  \      /   | /  ___/ | | |_   _| | | /  ___|     /   | | |     \e[0m\n"
+
+printf " \e[1;92m| |_| |    / /| | | |_| |     / /| | | |___  | |   | |   | | | |        / /| | | |     \e[0m\n"
+
+printf " \e[1;92m|  ___/   / / | | |  _  /    / / | | \___  \ | |   | |   | | | |       / / | | | |     \e[0m\n"
+
+printf "\e[1;92m | |      / /  | | | | \ \   / /  | |  ___| | | |   | |   | | | |___   / /  | | | |___  \e[0m\n"
+
+printf "\e[1;92m |_|     /_/   |_| |_|  \_\ /_/   |_| /_____/ |_|   |_|   |_| \_____| /_/   |_| |_____| \e[0m\n"
+
+printf "\e[1;94m Telegram : https://t.me/iiwiw \e[0m\n"
+
+printf "\e[1;94m github : https://github.com/iraq-hacker \e[0m\n"
+
+printf "\e[1;94m telegram channel : https://t.me/Professional_school \e[0m\n"
+
+}
