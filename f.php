365413a17f342eb2310b366f51bc4a95044822b3
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2020-12-09 09:07:10 +0300

    Update f.php

--- f.php
+++ f.php
@@ -1,2 +1,32 @@
 ?php
 $Shet
+
+
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+
+    {
+
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+
+    }
+
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+
+    {
+
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+
+    }
+
+else
+
+    {
+
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+
+    }
+
+$useragent = " User-Agent: ";
+
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
